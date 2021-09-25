a545508e2a1412987f67220c3a7aea5dd69ea201
Sapan Patibandha <patibandha@hotmail.com>
2021-09-25 08:26:43 +0530

    Base class implementation for services.

---
+++ WebClient/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebClient.Services
+{
+    public class BaseService
+    {
+        internal readonly HttpClient _client;
+
+        public BaseService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+
+            _client.DefaultRequestHeaders.Authorization
+             = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoicGF0aWJhbmRoYSIsIlVzZXIiOiJwYXRpYmFuZGhhIiwiaWQiOiJiODY3MTEwNi05YmE3LTQyN2EtYmE5ZC1hYmJiZmJiY2RhYjciLCJqdGkiOiJkN2YwZTE0Mi00ZmJjLTQ1N2YtOTI0ZC1jZjUzOWQ4OWI1MTUiLCJleHAiOjE2MzI1NDkzMjUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NjE5NTUiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAifQ.koMQrV1KTYbrTtDE53mWQVvq1mFj7bTDr6q5TgmkRRE");
+
+        }
+    }
+}
--- WebClient/Services/WeatherService.cs
+++ WebClient/Services/WeatherService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using WebClient.Extensions;
 using WebClient.Models;
@@ -9,15 +8,11 @@ using WebClient.Models;
 
 namespace WebClient.Services
 {
-    public class WeatherService : IWeatherService
+    public class WeatherService : BaseService, IWeatherService
     {
-        private readonly HttpClient _client;
-
-        public WeatherService(HttpClient client)
+        public WeatherService(HttpClient client) : base(client)
         {
-            _client = client ?? throw new ArgumentNullException(nameof(client));
-            _client.DefaultRequestHeaders.Authorization
-                         = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoicGF0aWJhbmRoYSIsIlVzZXIiOiJwYXRpYmFuZGhhIiwiaWQiOiJiODY3MTEwNi05YmE3LTQyN2EtYmE5ZC1hYmJiZmJiY2RhYjciLCJqdGkiOiIzYmEwNzkyOS1jNjRmLTQ2NjAtOWU2My01MGViNTNhN2FiNDEiLCJleHAiOjE2MzI1MTY3OTksImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NjE5NTUiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAifQ.LvkRES26l0JA3RkFBrL0GgUPvuCBL1lt5SWg2FrOKtk");
+
         }
 
         public async Task<IEnumerable<WeatherModel>> GetWeather()
